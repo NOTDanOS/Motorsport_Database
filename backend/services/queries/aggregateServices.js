53b7d3ed2a297fce04cfb3421be375ea0a1c306c
nazifishrak <nzfishrak60@gmail.com>
2025-03-31 16:45:40 -0700

    Attempt to fix incorrect import and typo in EngineerAssigment table

--- backend/services/queries/aggregateServices.js
+++ backend/services/queries/aggregateServices.js
@@ -1,45 +1,45 @@
 const { withOracleDB } = require("../../utils/oracleHelper");
 
 async function engineersPerTeam() {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(`
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(`
             SELECT ET.team_name, COUNT(EA.eng_id) AS num_engineers
             FROM Engineering_Team ET
             JOIN Engineer_Assignment EA ON ET.eng_team_id = EA.eng_team_id
             GROUP BY ET.team_name
             `);
-        return result.rows;
-    });
+    return result.rows;
+  });
 }
 
 async function teamsWithMultipleEngineers() {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(`
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(`
             SELECT ET.team_name, COUNT(EA.eng_id) AS num_engineers
             FROM Engineering_Team ET
             JOIN Engineer_Assignment EA ON ET.eng_team_id = EA.eng_team_id
             GROUP BY ET.team_name
             HAVING COUNT(EA.eng_id) > 1
             `);
-        return result.rows;
-    });
+    return result.rows;
+  });
 }
 
 async function experiencedEngineeringTeams() {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(`
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(`
             SELECT eng_team_id, AVG(years_experience) AS avg_experience
             FROM Engineer_Assignment
             GROUP BY eng_team_id
-            HAVING AVG(years_experience) > (SELECT AVG(years_experience) FROM Engineer_Assingment)
+            HAVING AVG(years_experience) > (SELECT AVG(years_experience) FROM Engineer_Assignment)
             `);
-        return result.rows;
-    });
+    return result.rows;
+  });
 }
 
 async function teamsInAllRacingSeries() {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(`
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(`
             SELECT T.team_id
             FROM Team T
             WHERE NOT EXISTS (SELECT RS.rs_id
@@ -49,13 +49,13 @@ async function teamsInAllRacingSeries() {
                               FROM Competes_In CI
                               WHERE CI.team_id = T.team_id)
             `);
-        return result.rows;
-    });
+    return result.rows;
+  });
 }
 
 module.exports = {
-    engineersPerTeam,
-    teamsWithMultipleEngineers,
-    experiencedEngineeringTeams,
-    teamsInAllRacingSeries
+  engineersPerTeam,
+  teamsWithMultipleEngineers,
+  experiencedEngineeringTeams,
+  teamsInAllRacingSeries,
 };
